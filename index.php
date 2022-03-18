7e072e78220e11e98010ef78e092a6ffc5d5b390
Theofilusarifin <theofilusarifin@gmail.com>
2022-03-18 08:30:26 +0700

    Backend Done

--- index.php
+++ index.php
@@ -25,22 +25,40 @@
             border: solid black 1px;
             padding: 30px 70px;
         }
+        .button_hasil{
+            <?php
+                if(isset($_POST)){
+                    foreach ($_POST as $key => $value) {
+                        if($value != ""){
+                            if($key == "box-shadow"){
+                                $shadow = $_POST["box-shadow"];
+                                if (count($shadow) == 3 && $shadow[0] != "" && $shadow[1] != "" && $shadow[2] == "") {
+                                    echo "        $key: $shadow[0]px $shadow[1]px;";
+                                }
+                                if (count($shadow) == 3 && $shadow[0] != "" && $shadow[1] != "" && $shadow[2] != "") {
+                                    echo "        $key: $shadow[0]px $shadow[1]px $shadow[2];";
+                                }
+                                echo "\n";
+                            }
+                            else{
+                                if(is_numeric($value)) $px = "px";
+                                else $px = "";
+                                echo "$key: $value$px; \n";
+                            }
+                        }
+                    }
+                }
+            ?>
+        }
     </style>
-
 </head>
-<?php
-    if(isset($_POST['submit'])){
-        $style = "Test";
-    }
-?>
-
 <body>
     <div class="container">
         <h1>Project Webprog</h1>
         <h4>Theofilus Arifin - 160420046</h4>
         <h4>Valencia - 160820016</h4>
         <br><br>
-        <form action="index.php" method="POST">
+        <form action="index.php" method="POST" style="margin-bottom: 30px;">
 
             <!-- Width -->
             <div class="prop">
@@ -48,12 +66,8 @@
                     Width : 
                 </div>
                 <div>
-                    <input type="number"> &nbsp; 
-                    <select name="" id="">
-                        <option value="px">px</option>
-                        <option value="em">em</option>
-                        <option value="rem">rem</option>
-                    </select>
+                    <input type="number" name="width"> &nbsp; 
+                    <label>px</label>
                 </div>
             </div>
 
@@ -63,13 +77,31 @@
                     Height : 
                 </div>
                 <div>
-                    <input type="number"> &nbsp; 
-                    <select name="" id="">
-                        <option value="px">px</option>
-                        <option value="em">em</option>
-                        <option value="rem">rem</option>
-                    </select>
+                    <input type="number" name="height"> &nbsp; 
+                    <label>px</label>
                 </div>
+            </div>
+
+            <!-- Padding -->
+            <div class="prop">
+                <div class="prop-sentence">
+                    Padding: 
+                </div>
+                <div>
+                    <input type="number" name="padding"> &nbsp; 
+                    <label>px</label> &nbsp;
+                </div> 
+            </div>
+
+            <!-- Margin -->
+            <div class="prop">
+                <div class="prop-sentence">
+                    Margin: 
+                </div>
+                <div>
+                    <input type="number" name="margin"> &nbsp; 
+                    <label>px</label> &nbsp;
+                </div> 
             </div>
 
             <!-- Text-align -->
@@ -78,7 +110,7 @@
                     Text-align :
                 </div>
                 <div>
-                    <select name="" id="">
+                    <select name="text-align">
                         <option value="right">right</option>
                         <option value="left">left</option>
                     </select>
@@ -91,7 +123,7 @@
                     Text-decoration:
                 </div>
                 <div>
-                    <select name="" id="">
+                    <select name="text-decoration">
                         <option value="none">none</option>
                         <option value="underline">underline</option>
                         <option value="overline">overline</option>
@@ -100,15 +132,15 @@
                 </div>
             </div>
 
-            <!-- Font-color -->
+            <!-- Color -->
             <div class="prop">
                 <div class="prop-sentence">
-                    Font-color : 
+                    Color : 
                 </div>
                 <div>
-                    <input type="text">
+                    <input type="text" name="color">
                 </div> 
-            </div>
+            </div>   
 
             <!-- Font-size -->
             <div class="prop">
@@ -116,11 +148,10 @@
                     Font-size : 
                 </div>
                 <div>
-                    <input type="number">
+                    <input type="number" name="font-size">
                     <label>px</label> &nbsp; 
                 </div> 
             </div>
-
 
             <!-- Background-color -->
             <div class="prop">
@@ -128,19 +159,9 @@
                     Background-color : 
                 </div>
                 <div>
-                    <input type="text">
+                    <input type="text" name="background-color">
                 </div> 
-            </div>
-
-            <!-- Color -->
-             <div class="prop">
-                <div class="prop-sentence">
-                    Color : 
-                </div>
-                <div>
-                    <input type="text">
-                </div> 
-            </div>           
+            </div>        
 
             <!-- Box Shadow -->
             <div class="prop">
@@ -148,13 +169,12 @@
                     Box-Shadow: 
                 </div>
                 <div>
-                    <input type="number"> &nbsp; 
-                    <input type="number"> &nbsp; 
-                    <input type="number"> &nbsp; 
+                    <input type="number" name="box-shadow[]"> &nbsp; 
+                    <input type="number" name="box-shadow[]"> &nbsp; 
                     <label>px</label> &nbsp; 
                 </div> 
                 <div>
-                    <input type="text">
+                    <input type="text" name="box-shadow[]">
                     <label>color</label>
                 </div>
             </div>
@@ -167,18 +187,18 @@
                     Border-radius: 
                 </div>
                 <div>
-                    <input type="number"> &nbsp; 
+                    <input type="number" name="border-radius"> &nbsp; 
                     <label>px</label> &nbsp; 
                 </div> 
             </div>
 
-             <!-- Border Color -->
-             <div class="prop">
+            <!-- Border Color -->
+            <div class="prop">
                 <div class="prop-sentence">
                     Border-color: 
                 </div>
                 <div>
-                    <input type="text"> &nbsp; 
+                    <input type="text" name="border-color"> &nbsp; 
                 </div> 
             </div>
 
@@ -188,7 +208,7 @@
                     Border-width:
                 </div>
                 <div>
-                    <select name="" id="">
+                    <select name="border-width">
                         <option value="thin">thin</option>
                         <option value="medium">medium</option>
                         <option value="thick">thick</option>
@@ -202,42 +222,55 @@
                     Cursor:
                 </div>
                 <div>
-                    <select name="" id="">
+                    <select name="cursor">
                         <option value="pointer">pointer</option>
                         <option value="not-allowed">not-allowed</option>
                     </select>
                 </div>
             </div>            
 
-            <!-- Padding -->
             <div class="prop">
-                <div class="prop-sentence">
-                    Padding: 
-                </div>
-                <div>
-                    <input type="number"> &nbsp; 
-                    <label>px</label> &nbsp; 
-                    <input type="number"> &nbsp; 
-                    <label>px</label> &nbsp; 
-                </div> 
+                <input type="submit"> &nbsp; &nbsp; &nbsp; 
+                <button onclick="location.reload()">Reset CSS</button>
             </div>
-            
-
-
-
-
-
-            <br>
-            <input type="submit" name="submit">
         </form>
-
         <p>CSS:</p>
-        <textarea name="css_textarea" id="" cols="30" rows="10" readonly>
+        <div style="border: solid black 1px;">
+            <?php
+                if(isset($_POST)){
+                    echo '<pre>';
+                    echo htmlspecialchars('   <style>');
+                    echo "<br>";
+                    foreach ($_POST as $key => $value) {
+                        if($value != ""){
+                            if($key == "box-shadow"){
+                                $shadow = $_POST["box-shadow"];
+                                if (count($shadow) == 2 && $shadow[0] != "" && $shadow[1] != "" && $shadow[2] == "") {
+                                    echo "        $key: $shadow[0]px $shadow[1]px;";
+                                    echo "<br>";
+                                }
+                                if (count($shadow) == 3 && $shadow[0] != "" && $shadow[1] != "" && $shadow[2] != "") {
+                                    echo "        $key: $shadow[0]px $shadow[1]px $shadow[2];";
+                                    echo "<br>";
+                                }                            
+                            }
+                            else{
+                                if(is_numeric($value)) $px = " px";
+                                else $px = "";
+                                echo "        $key: $value$px;";
+                                echo "<br>";
+                            }
+                        }
+                    }
+                    echo htmlspecialchars('   </style>');
+                    echo '</pre>';
+                }
+            ?>
+        </div>
 
-        </textarea>
 
         <p>Hasil Button:</p>
-        <button></button>
+        <button class="button_hasil">Button</button>
     </div>
 </body>
 </html>
